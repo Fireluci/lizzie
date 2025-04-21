9e4df76d90cbe9c8c2df8206f8851d388919e19a
Fireluci <120563797+Fireluci@users.noreply.github.com>
2025-04-21 15:05:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7016950483:AAG1StY7dAD3hoCE-2DshHYXiSCxxSVOMJs")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 APP_ID = int(os.environ.get("APP_ID", "24314601"))
 API_HASH = os.environ.get("API_HASH", "ede341e2d490a0fad5469866dedf8a95")
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002010704094"))
